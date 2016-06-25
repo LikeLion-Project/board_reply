1123ff80145a57a4b05a60b123293840586317a2
choijaewoong <jwchoi0129@gmail.com>
2016-06-25 08:20:40 +0000

    게시글 작성 기능 구현

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,14 @@ class HomeController < ApplicationController
     @post_list = Post.all.order("id desc")
     
   end
+  
+  def write_post    
+  end
+  def write      
+    post = Post.new(title: params[:title],
+                    content: params[:content])
+    post.save
+    
+    redirect_to "/home/index"
+  end
 end
